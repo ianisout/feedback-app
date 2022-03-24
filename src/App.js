034dd9e03164bea1b520309e60409db83bdf0fd2
Ian <lsouza.ian@gmail.com>
2022-03-24 09:20:45 -0300

    playing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
+import React from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './pages/AboutPage';
+import AboutIconLink from './components/AboutIconLink';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
-  const addFeedback = newFeedback => {
+  const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback])
-  }
+    setFeedback([newFeedback, ...feedback]);
+  };
 
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete this feedback?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
+      setFeedback(feedback.filter((item) => item.id !== id));
     }
-  }
+  };
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={ addFeedback } />
-        <FeedbackStats feedback={ feedback }/>
-        <FeedbackList feedback={ feedback } handleDelete={deleteFeedback}/>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+
+        <AboutIconLink />
       </div>
-    </>
+    </Router>
   );
 }
 
