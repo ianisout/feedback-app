e2cb295721d7f9546d5137977bbd869e4a70751d
Ian <lsouza.ian@gmail.com>
2022-03-24 05:58:47 -0300

    create and populate cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import Header from "./components/Header";
+import { useState } from 'react';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
   return (
     <>
-    <Header/>
-    <div className="container">
-      <h1>My App</h1>
-    </div>
+      <Header />
+      <div className="container">
+        <FeedbackList feedback={ feedback } />
+      </div>
     </>
   );
 }
