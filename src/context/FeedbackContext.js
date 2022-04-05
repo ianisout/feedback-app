df97ccdb2aaa2cf67d404775048624356211a1cc
Ian <lsouza.ian@gmail.com>
2022-04-05 09:25:03 -0300

    add back-end mockup w json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,25 @@
 import { v4 as uuidv4 } from 'uuid';
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "Terrible. I'm afraid he's Hitler's reincarnation",
-      rating: 1,
-    },
-    {
-      id: 2,
-      text: "She's in a good mood today, so, a hard 4. Most days she doesn't go past 2.",
-      rating: 4,
-    },
-    {
-      id: 3,
-      text: "Most amazing cat in the world! (It's looking at me menacingly, pls send help)",
-      rating: 10,
-    },
-  ]);
-
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  const fetchFeedback = async () => {
+    const response = await fetch(`http://localhost:3001/feedback`);
+    const data = await response.json();
+
+    setFeedback(data);
+  };
 
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
@@ -33,7 +27,11 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure you want to delete this feedback? Are you the cat reading this?')) {
+    if (
+      window.confirm(
+        'Are you sure you want to delete this feedback? Are you the cat reading this?'
+      )
+    ) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
